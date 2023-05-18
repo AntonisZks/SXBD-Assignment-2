95b235ac82563f81780c5861c689cc8920ffd0fd
AntonisZks <antoniszks2003@gmail.com>
2023-05-18 12:30:59 +0300

    Change from JOIN to WHERE clause

--- ΣΧΒΔ-2023_Άσκηση-2_taxidia/findRevenue.sql
+++ ΣΧΒΔ-2023_Άσκηση-2_taxidia/findRevenue.sql
@@ -1,4 +1,11 @@
--- Query 2: findRevenue
+-- Query 2: findRevenue | Order: DESC
+--
+--
+--
+-- Construction with JOIN
+--
+--
+--
 SELECT DISTINCT incomes_travel_agency_branch_id as travel_agency_branch_id, reservations AS total_num_reservations, SUM(income) AS total_income,
 	   num_of_employees, total_salary
 FROM (
@@ -23,5 +30,40 @@ FROM (
 	     JOIN travel_agency_branch tab ON e.travel_agency_branch_travel_agency_branch_id = tab.travel_agency_branch_id
 	  GROUP BY employees_travel_agency_brach_id
 	 ) travel_agency_branch_employees ON reservations_travel_agency_branch_id = travel_agency_branch_employees.employees_travel_agency_brach_id
+GROUP BY incomes_travel_agency_branch_id
+ORDER BY total_income DESC;
+
+--
+--
+--
+-- Construction with WHERE
+--
+--
+--
+SELECT DISTINCT incomes_travel_agency_branch_id as travel_agency_branch_id, reservations AS total_num_reservations, SUM(income) AS total_income,
+	   num_of_employees, total_salary
+FROM (
+	  -- Returns a table with all branches and their incomes
+	  SELECT tab.travel_agency_branch_id AS incomes_travel_agency_branch_id, COUNT(r.Reservation_id)*tp.cost_per_person as income
+	  FROM reservation r, travel_agency_branch tab, trip_package tp
+	  WHERE r.travel_agency_branch_id =  tab.travel_agency_branch_id AND r.offer_trip_package_id = tp.trip_package_id
+	  GROUP BY incomes_travel_agency_branch_id, tp.cost_per_person
+	 ) travel_agency_branch_incomes,
+     (
+      -- Returns a table all braches with their number of reservations
+      SELECT tab.travel_agency_branch_id AS reservations_travel_agency_branch_id, COUNT(r.Reservation_id) as reservations
+	  FROM reservation r, travel_agency_branch tab
+	  WHERE r.travel_agency_branch_id = tab.travel_agency_branch_id
+	  GROUP BY reservations_travel_agency_branch_id
+     ) travel_agency_branch_reservations,
+     (
+	  -- Returns a table with all branches and their number of employees and total salary
+      SELECT tab.travel_agency_branch_id AS employees_travel_agency_brach_id, COUNT(e.employees_AM) AS num_of_employees, SUM(e.salary) AS total_salary
+	  FROM employees e, travel_agency_branch tab
+	  WHERE e.travel_agency_branch_travel_agency_branch_id = tab.travel_agency_branch_id
+	  GROUP BY employees_travel_agency_brach_id
+	 ) travel_agency_branch_employees
+WHERE travel_agency_branch_incomes.incomes_travel_agency_branch_id = travel_agency_branch_reservations.reservations_travel_agency_branch_id
+  AND reservations_travel_agency_branch_id = travel_agency_branch_employees.employees_travel_agency_brach_id
 GROUP BY incomes_travel_agency_branch_id
 ORDER BY total_income DESC;
--- ΣΧΒΔ-2023_Άσκηση-2_taxidia/findTrips.sql
+++ ΣΧΒΔ-2023_Άσκηση-2_taxidia/findTrips.sql
@@ -1,11 +1,36 @@
 -- Query 1: findTrips | trip_package_id: 1, trip_start: 2019-06-01, trip_end: 2022-09-12
+--
+--
+--
+-- Construction with JOIN
+--
+--
+--
 SELECT tp.cost_per_person, tp.max_num_participants, 
 	   COUNT(*) as reservations, (tp.max_num_participants - COUNT(*)) as empty_seats,
-       CONCAT(e.name, ' ', e.surname) AS driver, tp.trip_start, tp.trip_end
+        CONCAT(e.name, ' ', e.surname) AS driver, tp.trip_start, tp.trip_end
 FROM reservation r 
 	 JOIN travel_agency_branch tab ON r.travel_agency_branch_id = tab.travel_agency_branch_id
 	 JOIN trip_package tp ON r.offer_trip_package_id = tp.trip_package_id
-     JOIN employees e ON e.travel_agency_branch_travel_agency_branch_id = tab.travel_agency_branch_id
-     JOIN drivers d ON d.driver_employee_AM = e.employees_AM
+      JOIN employees e ON e.travel_agency_branch_travel_agency_branch_id = tab.travel_agency_branch_id
+      JOIN drivers d ON d.driver_employee_AM = e.employees_AM
 WHERE tab.travel_agency_branch_id = 1 AND tp.trip_start BETWEEN '2019-06-01' AND '2022-09-12'
+GROUP BY tp.trip_package_id, tp.cost_per_person, tp.max_num_participants, d.driver_employee_AM, tp.trip_start, tp.trip_end
+
+--
+--
+--
+-- Construction with WHERE
+--
+--
+--
+SELECT tp.cost_per_person, tp.max_num_participants, 
+	   COUNT(*) as reservations, (tp.max_num_participants - COUNT(*)) as empty_seats,
+        CONCAT(e.name, ' ', e.surname) AS driver, tp.trip_start, tp.trip_end
+FROM reservation r, travel_agency_branch tab, trip_package tp, employees e, drivers d
+WHERE r.travel_agency_branch_id = tab.travel_agency_branch_id 
+  AND r.offer_trip_package_id = tp.trip_package_id
+  AND e.travel_agency_branch_travel_agency_branch_id = tab.travel_agency_branch_id
+  AND d.driver_employee_AM = e.employees_AM
+  AND tab.travel_agency_branch_id = 1 AND tp.trip_start BETWEEN '2019-06-01' AND '2022-09-12'
 GROUP BY tp.trip_package_id, tp.cost_per_person, tp.max_num_participants, d.driver_employee_AM, tp.trip_start, tp.trip_end
