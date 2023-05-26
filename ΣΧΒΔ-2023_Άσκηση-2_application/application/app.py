842a529aaa63e9af37c5fe56d3f3728e86528a39
AntonisZks <antoniszks2003@gmail.com>
2023-05-26 20:48:59 +0300

    ...

--- ΣΧΒΔ-2023_Άσκηση-2_application/application/app.py
+++ ΣΧΒΔ-2023_Άσκηση-2_application/application/app.py
@@ -86,14 +86,103 @@ def findRevenue(x):
 
 
 def bestClient(x):
-
     # Create a new connection
-    con = connection()
+    database = connection()
     # Create a cursor on the connection
-    cur = con.cursor()
+    cursor = database.cursor()
 
-    return [("first_name", "last_name", "total_countries_visited", "total_cities_visited", "list_of_attractions"), ]
+    best_client_query = """SELECT t.name, t.surname, t.traveler_id, SUM(tp.cost_per_person) AS total_cost
+                          FROM traveler t, reservation r, trip_package tp
+                          WHERE r.Customer_id = t.traveler_id
+                            AND r.offer_trip_package_id = tp.trip_package_id
+                          GROUP BY t.traveler_id
+                          ORDER BY total_cost DESC
+                          LIMIT 1;"""
+    
+    try:
+        cursor.execute(best_client_query)
+        best_client_data = list(cursor.fetchall())
+        best_client_id = best_client_data[0][2]
+        
+    except:
+        print("Error: unable to fetch data")
 
+    countries_and_cities_query = f"""SELECT cities_traveler_id AS traveler_id, name AS first_name, surname AS last_name, countries AS total_countries_visited, cities AS total_cities_visited
+                                    FROM
+                                        (
+                                        SELECT t.traveler_id AS cities_traveler_id, t.name, t.surname, COUNT(d.name) as cities
+                                        FROM reservation r, traveler t, trip_package tp, trip_package_has_destination tphd, destination d
+                                        WHERE r.Customer_id = t.traveler_id AND r.offer_trip_package_id = tp.trip_package_id
+                                        AND tphd.trip_package_trip_package_id = tp.trip_package_id AND tphd.destination_destination_id = d.destination_id
+                                        GROUP BY traveler_id
+                                        ) travelers_cities,
+                                        (
+                                        SELECT traveler_id AS countries_traveler_id, COUNT(country) AS countries
+                                        FROM
+                                            (
+                                            SELECT DISTINCT t.traveler_id, d.country
+                                            FROM reservation r, traveler t, trip_package tp, trip_package_has_destination tphd, destination d
+                                            WHERE r.Customer_id = t.traveler_id AND r.offer_trip_package_id = tp.trip_package_id
+                                            AND tphd.trip_package_trip_package_id = tp.trip_package_id
+                                            AND tphd.destination_destination_id = d.destination_id
+                                            ) travelers_countries
+                                        GROUP BY traveler_id
+                                        ) travelers_countries
+                                    WHERE travelers_cities.cities_traveler_id = travelers_countries.countries_traveler_id
+                                    HAVING traveler_id = {best_client_id};"""
+
+    try:
+        cursor.execute(countries_and_cities_query)
+        countries_and_cities_result = list(cursor.fetchall())
+        
+    except:
+        print("Error: unable to fetch data")
+    
+
+    tourist_attractions_query = f"""SELECT DISTINCT traveler_id as client, city, country, tourist_attraction
+                                    FROM
+                                        (
+                                        SELECT t.traveler_id as traveler_id, r.Reservation_id as travelers_reservations_id
+                                        FROM reservation r, traveler t
+                                        WHERE r.Customer_id = t.traveler_id
+                                        ORDER BY t.traveler_id
+                                        ) travels_reserevations,
+                                        (
+                                        SELECT r.Reservation_id as trips_reservations_id, tp.trip_package_id as trip_package_id
+                                        FROM reservation r, trip_package tp
+                                        WHERE r.offer_trip_package_id = tp.trip_package_id
+                                        ORDER BY r.Reservation_id
+                                        ) reservations_trips,
+                                        (
+                                        SELECT tp.trip_package_id as destinations_trip_package_id, d.name as city, d.country, ta.name as tourist_attraction
+                                        FROM trip_package_has_destination tphd, trip_package tp, destination d, tourist_attraction ta
+                                        WHERE tphd.trip_package_trip_package_id = tp.trip_package_id
+                                        AND tphd.destination_destination_id = d.destination_id
+                                        AND ta.destination_destination_id = d.destination_id
+                                        ORDER BY tp.trip_package_id
+                                        ) trips_destinations
+                                    WHERE travels_reserevations.travelers_reservations_id = reservations_trips.trips_reservations_id
+                                    AND reservations_trips.trip_package_id = trips_destinations.destinations_trip_package_id
+                                    HAVING client = {best_client_id};"""
+
+    try:
+        cursor.execute(tourist_attractions_query)
+        tourist_attractions_result = list(cursor.fetchall())
+        
+    except:
+        print("Error: unable to fetch data")
+
+
+    print(best_client_data)
+    print(countries_and_cities_result)
+    print(tourist_attractions_result)
+
+    result_list = [("first_name", "last_name", "total_countries_visited", "total_cities_visited", "list_of_attractions")]
+    for i in range(len(tourist_attractions_result)):
+        row = (countries_and_cities_result[0][1], countries_and_cities_result[0][2], countries_and_cities_result[0][3], countries_and_cities_result[0][4], tourist_attractions_result[i][3])
+        result_list.append(row)
+    
+    return result_list
 
 def giveAway(N):
     # Create a new connection
--- ΣΧΒΔ-2023_Άσκηση-2_taxidia/findTrips.sql
+++ ΣΧΒΔ-2023_Άσκηση-2_taxidia/findTrips.sql
@@ -1,29 +1,3 @@
--- Query 1: findTrips | trip_package_id: 1, trip_start: 2019-06-01, trip_end: 2022-09-12
---
---
---
--- Construction with JOIN
---
---
---
-SELECT tp.cost_per_person, tp.max_num_participants, 
-	   COUNT(*) as reservations, (tp.max_num_participants - COUNT(*)) as empty_seats,
-        CONCAT(e.name, ' ', e.surname) AS driver, tp.trip_start, tp.trip_end
-FROM reservation r 
-	 JOIN travel_agency_branch tab ON r.travel_agency_branch_id = tab.travel_agency_branch_id
-	 JOIN trip_package tp ON r.offer_trip_package_id = tp.trip_package_id
-      JOIN employees e ON e.travel_agency_branch_travel_agency_branch_id = tab.travel_agency_branch_id
-      JOIN drivers d ON d.driver_employee_AM = e.employees_AM
-WHERE tab.travel_agency_branch_id = 1 AND tp.trip_start BETWEEN '2019-06-01' AND '2022-09-12'
-GROUP BY tp.trip_package_id, tp.cost_per_person, tp.max_num_participants, d.driver_employee_AM, tp.trip_start, tp.trip_end;
-
---
---
---
--- Construction with WHERE
---
---
---
 SELECT tp.cost_per_person, tp.max_num_participants, 
 	   COUNT(*) as reservations, (tp.max_num_participants - COUNT(*)) as empty_seats,
 	   CONCAT(e.name, ' ', e.surname) AS travel_guide, tp.trip_start, tp.trip_end
