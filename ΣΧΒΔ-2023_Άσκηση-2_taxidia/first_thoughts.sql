e7e454f099200cc304d8a051a831e2f03cb1fc9e
AntonisZks <antoniszks2003@gmail.com>
2023-05-30 17:22:57 +0300

    fixed query1

--- ΣΧΒΔ-2023_Άσκηση-2_taxidia/first_thoughts.sql
+++ ΣΧΒΔ-2023_Άσκηση-2_taxidia/first_thoughts.sql
@@ -1,32 +1,29 @@
 /* ΜΕΡΟΣ Α */
 
 /*1. Βρείτε τα ονόματα των ξεναγών που έχουν χρησιμοποιηθεί από το πρακτορείο σε όλους τους προορισμούς της Γερμανίας. */
-SELECT CONCAT(em.name, ' ', em.surname) as full_name
-FROM employees em, travel_guide tg, guided_tour gt, trip_package tp, trip_package_has_destination tphd, destination d
-WHERE em.employees_AM = tg.travel_guide_employee_AM 
-      AND gt.travel_guide_employee_AM = tg.travel_guide_employee_AM
-      AND gt.trip_package_id = tp.trip_package_id
-      AND tp.trip_package_id = tphd.trip_package_trip_package_id
-      AND tphd.destination_destination_id = d.destination_id
-      AND d.country = "Germany"
-GROUP BY em.name, em.surname;
+SELECT DISTINCT e.name, e.surname
+FROM travel_guide tg, employees e, guided_tour gt, tourist_attraction ta, destination d
+WHERE tg.travel_guide_employee_AM = e.employees_AM
+  AND tg.travel_guide_employee_AM = gt.travel_guide_employee_AM
+  AND gt.tourist_attraction_id = ta.tourist_attraction_id
+  AND ta.destination_destination_id = d.destination_id
+  AND d.country = 'Germany'
+ORDER BY e.name;
 
 /* 2. Βρείτε τους κωδικούς των ξεναγών με περισσότερες από 3 ξεναγήσεις στο έτος 2019.*/
-SELECT tg.travel_guide_employee_AM, COUNT(gt.trip_package_id) AS tours
+SELECT tg.travel_guide_employee_AM AS travel_guide_id, COUNT(gt.trip_package_id) AS number_of_guided_tours
 FROM travel_guide tg, guided_tour gt, trip_package tp
 WHERE tg.travel_guide_employee_AM = gt.travel_guide_employee_AM
   AND gt.trip_package_id = tp.trip_package_id
   AND tp.trip_start >= '2019-01-01' AND tp.trip_start <= '2019-12-31'
 GROUP BY tg.travel_guide_employee_AM
-HAVING tours > 3
-ORDER BY tg.travel_guide_employee_AM;
+HAVING number_of_guided_tours > 3;
 
 /* 3. Βρείτε τον αριθμό των υπαλλήλων που έχει κάθε υποκατάστημα του ταξιδιωτικού πρακτορείου.*/
-SELECT tab.travel_agency_branch_id, COUNT(e.employees_AM) AS num_employees
+SELECT tab.travel_agency_branch_id, COUNT(e.employees_AM) AS number_of_employees
 FROM employees e, travel_agency_branch tab
 WHERE tab.travel_agency_branch_id = e.travel_agency_branch_travel_agency_branch_id
 GROUP BY tab.travel_agency_branch_id;
-
 
 /* 4. Βρείτε τα ταξιδιωτικά πακέτα και τον αριθμό των κρατήσεων που έγιναν σε αυτά στο διάστημα '2021-01-01' έως '2021-12-31' με προορισμό το Παρίσι.*/
 SELECT tp.trip_package_id, COUNT(r.Reservation_id) AS reservations
@@ -36,8 +33,7 @@ WHERE r.offer_trip_package_id = tp.trip_package_id
   AND tphd.destination_destination_id = d.destination_id
   AND r.date >= '2021-01-01' AND r.date <= '2021-12-31'
   AND d.name = 'Paris'
-GROUP BY tp.trip_package_id
-ORDER BY tp.trip_package_id;
+GROUP BY tp.trip_package_id;
 
 /* 5. Βρείτε τους ξεναγούς που έχουν κάνει όλες τις ξεναγήσεις στην ίδια γλώσσα. */
 SELECT e.name, e.surname
@@ -50,7 +46,6 @@ FROM
 		SELECT DISTINCT tg.travel_guide_employee_AM, gt.travel_guide_language_id
 		FROM travel_guide tg, guided_tour gt
 		WHERE tg.travel_guide_employee_AM = gt.travel_guide_employee_AM
-		ORDER BY tg.travel_guide_employee_AM
 		) travel_guide_tour_languages
 	GROUP BY travel_guide_employee_AM
 	HAVING languages = 1
@@ -79,7 +74,7 @@ FROM (
 ) AS result_table;
 
 /* 7. Βρείτε όλους τους άντρες ταξιδιώτες που έχουν ηλικία από 40 και πάνω κι έχουν κάνει κρατήσεις σε περισσότερα από 3 ταξιδιωτικά πακέτα.*/
-SELECT t.traveler_id, t.name, t.surname
+SELECT t.name, t.surname
 FROM traveler t
 WHERE t.gender = 'male'
   AND t.age >= 40
@@ -91,7 +86,7 @@ WHERE t.gender = 'male'
 GROUP BY t.traveler_id, t.name, t.surname;
 
 /* 8. Βρείτε τα ονόματα των ξεναγών που μιλάνε Αγγλικά και τον αριθμό των τουριστικών αξιοθέατων που έχει γίνει κάποια ξενάγηση από τον καθένα ξεναγό στην παραπάνω γλώσσα. */
-SELECT tg.travel_guide_employee_AM, em.name, em.surname, COUNT(ta.tourist_attraction_id) as num_of_attractions
+SELECT tg.travel_guide_employee_AM AS travel_guide_id, em.name, em.surname, COUNT(ta.tourist_attraction_id) as number_of_attractions
 FROM employees em, travel_guide tg, travel_guide_has_languages tghl, tourist_attraction ta, guided_tour gt
 WHERE tg.travel_guide_employee_AM = tghl.travel_guide_employee_AM
   AND    em.employees_AM= tg.travel_guide_employee_AM
